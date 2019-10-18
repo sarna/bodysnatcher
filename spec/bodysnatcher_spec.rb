7462a92b01e530aed5c8d4a5a740679e5f828aec
leeraaz17 <liraj@banbagroup.com>
2019-10-18 23:03:54 +0545

    Initializing the specs for all the methods.

--- spec/bodysnatcher_spec.rb
+++ spec/bodysnatcher_spec.rb
@@ -1,11 +1,55 @@
 # frozen_string_literal: true
+require "spec_helper"
 
-RSpec.describe Bodysnatcher do
+RSpec.describe Bodysnatcher, type: :model do
   it 'has a version number' do
-    expect(Bodysnatcher::VERSION).not_to be nil
+    expect(Bodysnatcher::VERSION).to eq "0.1.0"
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  describe Bodysnatcher::Link do
+  	let(:attr) do
+      { 
+      	url: "hello.com",
+      	origin: "this"
+     	}
+  	end
+
+	  describe '#initialize' do
+	    it 'should get correct params' do
+	    	
+	    end
+  	end
+
+	  describe '#has' do
+	    it 'should define hash' do
+	    	
+	    end
+  	end
+
+	  describe '#eql' do
+	    it 'should be eqal with other hash' do
+	    	
+	    end
+  	end
+	end
+
+	describe Bodysnatcher::Walker do
+	  describe '#initialize' do
+	    it 'should get correct params' do
+	    	
+	    end
+  	end
+
+	  describe '#walk' do
+	    it 'should define hash' do
+	    	
+	    end
+  	end
+
+	  describe '#eql' do
+	    it 'should be eqal with other hash' do
+	    	
+	    end
+  	end
+	end
 end
